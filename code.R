5ccdf09df1f3157e84fe79aa14b8c50f958137fb
hzhu3835 <hzhu3835@uni.sydney.edu.au>
2021-10-20 13:08:59 +0100

    initial function

--- code.R
+++ code.R
@@ -1,24 +1,25 @@
-seir <- function(n=10000,ne=10,nt=100,gamma=1/3,delta=1/5,bmu=5e-5,bsc=1e-5) {
+set.seed(2)
+
+seir <- function(n=5.5e6,ne=10,nt=150,gamma=1/3,delta=1/5) {
   ## SEIR stochastic simulation model.
   ## n: population size; ne: initially exposed; nt: number of days 
+  ## beta: daily prob S -> E parameter; lamb: daily prob S -> E parameter
   ## gamma: daily prob E -> I
-  ## beta: daily prob S -> E parameter;  = daily prob I -> R;
-  ## bmu = mean beta; bsc = var(beta) = bmu * bsc
-  x <- rep(0,n) ## initialize to susceptible state
+  ## delta: daily prob I -> R
   
   beta <- rlnorm(n,0,0.5); beta <- beta/mean(beta) ## individual infection rates 
   lamb <- 0.4/n
   
+  x <- rep(0,n) ##initialize to susceptible state
   x[1:ne] <- 1 ## create some exposed
   S <- E <- I <- R <- rep(0,nt) ## set up storage for pop in each state 
-  S[1] <- n-ni; I[1] <- ni ## initialize
+  S[1] <- n-ne; E[1] <- ne ## initialize
   
   for (i in 2:nt) { ## loop over days
     u <- runif(n) ## uniform random deviates
     x[x==2&u<delta] <- 3 ## I -> R with prob delta 
     x[x==1&u<gamma] <- 2 ## E -> I with prob gamma 
-    x[x==0&u<beta*I[i-1]] <- 1 ## S -> E with prob 
-    beta*I[i-1] 
+    x[x==0&u[x==0]<lamb*beta[x==0]*sum(beta[x==2])] <- 1 ## S -> E with prob lamb*beta.j*sum(beta.i)
     S[i] <- sum(x==0); E[i] <- sum(x==1)
     I[i] <- sum(x==2); R[i] <- sum(x==3)
   }
